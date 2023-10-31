054b4d9d2fe04df38cd1218c02eeb973f626ef1a
mathurakshat <91469568+mathurakshat@users.noreply.github.com>
2023-10-31 10:47:51 +0530

    added test cases

--- angular-ngx-pagination-demo/src/app/home/home.component.spec.ts
+++ angular-ngx-pagination-demo/src/app/home/home.component.spec.ts
@@ -1,21 +1,86 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HomeComponent } from './home.component';
+import { CarsDataService } from '../services/cars-data.service';
+import { of } from 'rxjs';
+import { CarDetails } from '../models/cars-details.model';
+import { HttpClientModule } from '@angular/common/http';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { CarsdataCardComponent } from './carsdata-card/carsdata-card.component';
+import { MaterialModule } from '../modules/material.module';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let carsDataService: jasmine.SpyObj<CarsDataService>;
 
   beforeEach(() => {
+    const carsDataServiceSpy = jasmine.createSpyObj('CarsDataService', [
+      'getCarModels',
+    ]);
+
     TestBed.configureTestingModule({
-      declarations: [HomeComponent]
+      declarations: [HomeComponent, CarsdataCardComponent],
+      providers: [{ provide: CarsDataService, useValue: carsDataServiceSpy }],
+      imports: [
+        HttpClientModule, // Add HttpClientModule to the imports
+        NgxPaginationModule,
+        MaterialModule,
+      ],
     });
+
+    carsDataService = TestBed.inject(
+      CarsDataService,
+    ) as jasmine.SpyObj<CarsDataService>;
+    carsDataService.getCarModels.and.returnValue(of([]));
+
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call getCarModels on initialization', () => {
+    fixture.detectChanges();
+    expect(carsDataService.getCarModels).toHaveBeenCalled();
+  });
+
+  it('should update carModelDetails on successful getCarModels', () => {
+    const carModels: CarDetails[] = [
+      {
+        carId: 1,
+        brand: 'Chevrolet',
+        model: 'Express 3500',
+        year: 1998,
+        color: 'Fuscia',
+        mileage: 24,
+        price: '$7.68',
+        location: 'Shanghu',
+      },
+      {
+        carId: 2,
+        brand: 'Chevrolet',
+        model: 'Corvette',
+        year: 1978,
+        color: 'Mauv',
+        mileage: 21,
+        price: '$9.61',
+        location: 'Sarankhola',
+      },
+    ];
+    carsDataService.getCarModels.and.returnValue(of(carModels));
+
+    fixture.detectChanges();
+    expect(component.carModelDetails).toEqual(carModels);
+  });
+
+  it('should update page and call getCarModels on onTableDataChange', () => {
+    const newPage = 2;
+
+    component.onTableDataChange(newPage);
+
+    expect(component.page).toEqual(newPage);
+    expect(carsDataService.getCarModels).toHaveBeenCalled();
   });
 });
--- angular-ngx-pagination-demo/src/app/services/cars-data.service.spec.ts
+++ angular-ngx-pagination-demo/src/app/services/cars-data.service.spec.ts
@@ -1,16 +1,69 @@
 import { TestBed } from '@angular/core/testing';
-
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { CarsDataService } from './cars-data.service';
+import { CarDetails } from '../models/cars-details.model';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('CarsDataService', () => {
   let service: CarsDataService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, HttpClientModule],
+      providers: [CarsDataService],
+    });
+
+    // Inject the service and test controller for HTTP testing
     service = TestBed.inject(CarsDataService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should fetch car models data from JSON file', () => {
+    const mockCarData: CarDetails[] = [
+      {
+        carId: 1,
+        brand: 'Chevrolet',
+        model: 'Express 3500',
+        year: 1998,
+        color: 'Fuscia',
+        mileage: 24,
+        price: '$7.68',
+        location: 'Shanghu',
+      },
+      {
+        carId: 2,
+        brand: 'Chevrolet',
+        model: 'Corvette',
+        year: 1978,
+        color: 'Mauv',
+        mileage: 21,
+        price: '$9.61',
+        location: 'Sarankhola',
+      },
+    ];
+
+    service.getCarModels().subscribe((data) => {
+      expect(data).toEqual(mockCarData);
+    });
+
+    const req = httpTestingController.expectOne('assets/cardata.json');
+    expect(req.request.method).toBe('GET');
+
+    req.flush(mockCarData);
+
+    httpTestingController.verify();
+  });
+
+  afterEach(() => {
+    // After each test, make sure there are no outstanding requests
+    httpTestingController.verify();
   });
 });
